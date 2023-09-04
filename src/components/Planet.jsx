ea932db8417a9660d02a07f33c0696790ed285c7
SabaMuladze <muladze66@bk.ru>
2023-09-04 17:12:34 +0400

    style: create buttons for mobile

--- src/components/Planet.jsx
+++ src/components/Planet.jsx
@@ -1,37 +1,46 @@
 const Planet = ({ data }) => {
     return (
-        <main className="xl: px-[165px]">
-            <div className="flex items-center justify-center m-0  max-lg: max-h-[400px]">
-                <img src={data.images.planet} className=" max-lg:scale-50  lg:scale-75 " alt={data.name} />
+        <>
+            <div className="w-full flex justify-center border-b-[1px] border-solid  border-gray-600 pb-5 md:hidden">
+                <ul className="flex gap-10">
+                    <li className="font-sans">OVERVIEW</li>
+                    <li className="font-sans">STRUCTURE</li>
+                    <li className="font-sans">SURFACE</li>
+                </ul>
             </div>
-
-            <div className="max-md:flex flex-col justify-center items-center px-6  md:hidden">
-                <h1 className='font-serif text-[40px] uppercase '>{data.name}</h1>
-                <p className="text-center font-sans mt-4 leading-6 font-normal">{data.overview.content}</p>
-                <p className="font-sans mt-7 opacity-75">Source: <a className="font-sans underline decoration-1" target="_blank" href={data.overview.source}>Wikipedia</a></p>
-            </div>
-
-            <div className="flex max-md:flex-col max-md:items-center px-6 gap-3 mt-7 pb-5 ">
-                <div className="flex justify-between w-full px-6 py-4 border-[1px]  border-solid border-white/50 items-center md:flex-col md:justify-normal md:items-start lg:py-8 lg:pt-6">
-                    <p className="font-sans opacity-75 text-[10px] lg:text-[12px]">ROTATION TIME</p>
-                    <p className="font-serif text-xl md:text-2xl lg:text-4xl ">{data.rotation}</p>
+            <main className="xl:px-[165px]">
+                <div className="flex items-center justify-center m-0  max-lg: max-h-[400px]">
+                    <img src={data.images.planet} className=" max-lg:scale-50  lg:scale-75 " alt={data.name} />
                 </div>
-                <div className="flex justify-between w-full px-6 py-4 border-[1px]  border-solid border-white/50 items-center md:flex-col md:justify-normal md:items-start lg:py-8 lg:pt-6">
-                    <p className="font-sans opacity-75 text-[10px] lg:text-[12px]">REVOLUTION TIME</p>
-                    <p className="font-serif text-xl md:text-2xl lg:text-4xl ">{data.revolution}</p>
+
+                <div className="max-md:flex flex-col justify-center items-center px-6  md:hidden">
+                    <h1 className='font-serif text-[40px] uppercase '>{data.name}</h1>
+                    <p className="text-center font-sans mt-4 leading-6 font-normal">{data.overview.content}</p>
+                    <p className="font-sans mt-7 opacity-75">Source: <a className="font-sans underline decoration-1" target="_blank" href={data.overview.source}>Wikipedia</a></p>
                 </div>
-                <div className="flex justify-between w-full px-6 py-4 border-[1px]  border-solid border-white/50 items-center md:flex-col md:justify-normal md:items-start lg:py-8 lg:pt-6">
-                    <p className="font-sans opacity-75 text-[10px] lg:text-[12px]">RADIUS</p>
-                    <p className="font-serif text-xl md:text-2xl lg:text-4xl ">{data.radius}</p>
+
+                <div className="flex max-md:flex-col max-md:items-center px-6 gap-3 mt-7 pb-5 ">
+                    <div className="flex justify-between w-full px-6 py-4 border-[1px]  border-solid border-white/50 items-center md:flex-col md:justify-normal md:items-start lg:py-8 lg:pt-6">
+                        <p className="font-sans opacity-75 text-[10px] lg:text-[12px]">ROTATION TIME</p>
+                        <p className="font-serif text-xl md:text-2xl lg:text-4xl ">{data.rotation}</p>
+                    </div>
+                    <div className="flex justify-between w-full px-6 py-4 border-[1px]  border-solid border-white/50 items-center md:flex-col md:justify-normal md:items-start lg:py-8 lg:pt-6">
+                        <p className="font-sans opacity-75 text-[10px] lg:text-[12px]">REVOLUTION TIME</p>
+                        <p className="font-serif text-xl md:text-2xl lg:text-4xl ">{data.revolution}</p>
+                    </div>
+                    <div className="flex justify-between w-full px-6 py-4 border-[1px]  border-solid border-white/50 items-center md:flex-col md:justify-normal md:items-start lg:py-8 lg:pt-6">
+                        <p className="font-sans opacity-75 text-[10px] lg:text-[12px]">RADIUS</p>
+                        <p className="font-serif text-xl md:text-2xl lg:text-4xl ">{data.radius}</p>
+                    </div>
+                    <div className="flex justify-between w-full px-6 py-4 border-[1px]  border-solid border-white/50 items-center md:flex-col md:justify-normal md:items-start lg:py-8 lg:pt-6">
+                        <p className="font-sans opacity-75 text-[10px] lg:text-[12px]">AVERAGE TEMP.</p>
+                        <p className="font-serif text-xl md:text-2xl lg:text-4xl ">{data.temperature}</p>
+                    </div>
                 </div>
-                <div className="flex justify-between w-full px-6 py-4 border-[1px]  border-solid border-white/50 items-center md:flex-col md:justify-normal md:items-start lg:py-8 lg:pt-6">
-                    <p className="font-sans opacity-75 text-[10px] lg:text-[12px]">AVERAGE TEMP.</p>
-                    <p className="font-serif text-xl md:text-2xl lg:text-4xl ">{data.temperature}</p>
-                </div>
-            </div>
 
 
-        </main>
+            </main>
+        </>
     )
 }
 
