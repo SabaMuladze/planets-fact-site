47af1fe3441155a94842d659893abe0933b6a167
SabaMuladze <muladze66@bk.ru>
2023-09-04 12:52:28 +0400

    fix: planet img parent div height

--- src/components/Planet.jsx
+++ src/components/Planet.jsx
@@ -1,7 +1,7 @@
 const Planet = ({ data }) => {
     return (
         <main>
-            <div className="flex items-center justify-center m-0">
+            <div className="flex items-center justify-center m-0 max-lg: max-h-[400px]">
                 <img src={data.images.planet} className=" max-lg:scale-50  lg:scale-75 " alt={data.name} />
             </div>
 
